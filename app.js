75a1e9fd8161d2385230a7fb14450f1616084fb0
ubuntu <kashishgupta1990@yahoo.com>
2017-02-16 12:51:56 +0530

    number convert 1st step done.

---
+++ app.js
@@ -0,0 +1,62 @@
+var fs = require('fs'),
+    readline = require('readline'),
+    config = require('./config.json');
+var counter = 0;
+var numberListCounter = 0;
+var digitalNumberString = [];
+var resultNumberList = [];
+var tmpBit = {};
+var rd = readline.createInterface({
+    input: fs.createReadStream('./test/input.txt')
+});
+
+rd.on('line', function (line) {
+    if (line.trim() == '') {
+        digitalNumberString.push(tmpBit);
+        counter = 0;
+        numberListCounter++;
+        tmpBit = {};
+    } else {
+        tmpBit[counter] = line;
+        counter++;
+    }
+});
+
+rd.on('close', function () {
+    digitalNumberString.forEach(function (numSymbol) {
+        resultNumberList.push(convertDigitalFigureToNumber(numSymbol));
+    });
+    console.log(resultNumberList);
+});
+
+function convertDigitalFigureToNumber(digitalNumbers) {
+    var len = 27;
+    var counter = 0;
+    var str = '';
+    var finalNumber = '';
+    for (var num = 0; num < len; num++) {
+        if(digitalNumbers[0][num] || digitalNumbers[0][num] === 0){
+            str += digitalNumbers[0][num];
+        }else{
+            str += ' ';
+        }
+        str += digitalNumbers[1][num] +
+            digitalNumbers[2][num];
+        if (counter == 2) {
+            counter = 0;
+            finalNumber+=getNumber(str);
+            str = '';
+        }else{
+            counter++;
+        }
+    }
+    return finalNumber;
+}
+
+function getNumber(digitSymbol) {
+    if(config[digitSymbol] || config[digitSymbol] === 0){
+        return config[digitSymbol];
+    }else{
+        return digitSymbol;
+    }
+}
